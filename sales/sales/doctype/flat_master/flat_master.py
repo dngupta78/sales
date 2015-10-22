225eb40f32d60b253fca6c8115df0aaa6f4cede4
dngupta78 <dngupta78@gmail.com>
2015-10-22 18:06:23 +0530

    Update flat_master.py

--- sales/sales/doctype/flat_master/flat_master.py
+++ sales/sales/doctype/flat_master/flat_master.py
@@ -7,4 +7,38 @@ import frappe
 from frappe.model.document import Document
 
 class FlatMaster(Document):
-	pass
+	
+
+
+	def insertItem(self):
+		im=frappe.get_doc({      
+		"doctype": "Item",
+		"item_code":self.flat_no,
+		"item_name":self.flat_no,
+		"item_group":"All Item Group",
+		"default_warehouse":"Stores-TGR",
+
+		}).insert
+	
+
+
+
+
+#Insert Same Item in to the Item master
+@frappe.whitelist(allow_guest=True)
+def saveItem(self,method):
+	frappe.msgprint("Item")
+	'''im=frappe.get_doc({      
+		"doctype": "Item",
+		"item_code":self.flat_no,
+		"item_name":self.flat_no,
+		"item_group":"All Item Group",
+		"default_warehouse":"Stores-TGR",
+
+		}).insert
+	im.save()
+
+	#im.submit()
+'''
+
+
